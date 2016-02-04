0491acc06573b52112f8ca094b034c00fd49d23e
Jay Shah <Jay@jays-macbook-pro.local>
2016-02-03 23:50:15 -0500

    Complete

--- HW #1/ViewController.swift
+++ HW #1/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let navy = UIColor(red: 0, green: 0, blue: 128/255, alpha: 1)
+    let pink = UIColor(red: 1, green: 153/255, blue: 204/255, alpha: 1)
+    
+    @IBOutlet var largeView: UIView!
+
+    @IBAction func buttonClicked(sender: AnyObject) {
+        
+        largeView.backgroundColor = UIColor.greenColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func colorNavy(sender: AnyObject) {
+        largeView.backgroundColor = navy
     }
-
-
+    
+    @IBAction func colorPink(sender: AnyObject) {
+        largeView.backgroundColor = pink
+    }
+    
+    @IBOutlet weak var inputName: UITextField!
+    
+    @IBOutlet weak var outputPhrase: UITextView!
+    
+    @IBAction func buttonSubmit(sender: AnyObject) {
+        outputPhrase.text = "Hello " + inputName.text! + ", how are you?"
+    }
 }
 
