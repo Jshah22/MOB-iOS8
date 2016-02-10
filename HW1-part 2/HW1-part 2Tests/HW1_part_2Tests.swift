25305f2e6ec7db447e3d185ece8e07ebf9eef669
Jshah22 <Jay@Jays-MacBook-Pro.local>
2016-02-09 20:23:56 -0500

    HW1-part2
    part 2 added

---
+++ HW1-part 2/HW1-part 2Tests/HW1_part_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HW1_part_2Tests.swift
+//  HW1-part 2Tests
+//
+//  Created by Jay Shah on 2/9/16.
+//  Copyright © 2016 Jay Shah. All rights reserved.
+//
+
+import XCTest
+@testable import HW1_part_2
+
+class HW1_part_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
